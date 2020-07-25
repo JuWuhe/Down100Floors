c8986943cd8531737fc5f53575ed1bbe7977dda9
JuWuhe <Insyent@foxmail.com>
2020-07-25 17:22:36 +0800

    背景搭建

---
+++ Assets/Scripts/AnimationBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationBG : MonoBehaviour
+{
+    Material material;
+    Vector2 movement;
+
+    public Vector2 speed;
+
+    void Start()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    void Update()
+    {
+        movement += speed * Time.deltaTime;
+        material.mainTextureOffset = movement;
+    }
+}
