04096eb81f4155b454c4673ebed04e0f8a8b0677
JuWuhe <Insyent@foxmail.com>
2020-08-11 11:53:28 +0800

    Add Line

---
+++ Assets/Scripts/LineRender.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineRender : MonoBehaviour
+{
+    LineRenderer line;
+    public Transform startPoint;
+    public Transform endPoint;
+    void Start()
+    {
+        line = GetComponent<LineRenderer>();
+    }
+
+    
+    void Update()
+    {
+        line.SetPosition(0,startPoint.position);
+        line.SetPosition(1, endPoint.position);
+    }
+}
