1cce0125ae9ae8e491763dfadd99b05b820abec1
JuWuhe <Insyent@foxmail.com>
2020-08-12 11:21:25 +0800

    随机生成平台

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public List<GameObject> platforms = new List<GameObject>();
+
+    //控制随机生成间隔时间
+    public float spawnTime; 
+    public float countTime;
+
+    private Vector3 spawnPosition;
+
+    int spikeNum = 0;
+    
+    void Update()
+    {
+        SpawnPlatform();
+    }
+
+    public void SpawnPlatform()
+    {
+        countTime += Time.deltaTime;
+        spawnPosition = transform.position;
+        spawnPosition.x = Random.Range(-3f, 3f);  //具体数值具体调整
+
+        if(countTime >= spawnTime)
+        {
+            CreatPlatform();
+            countTime = 0;
+        }
+
+    }
+
+    public void CreatPlatform()
+    {
+        int index = Random.Range(0, platforms.Count);
+        if(index == 4)
+        {
+            spikeNum++;
+        }
+        if(spikeNum > 1)
+        {
+            spikeNum = 0;
+            countTime = spawnTime;
+            return;
+        }
+        GameObject newPlatform = Instantiate(platforms[index], spawnPosition, Quaternion.identity);
+        newPlatform.transform.SetParent(this.gameObject.transform);
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # Down100Floors
 真男人就下一百层
 
-Unity照抄小游戏
+Unity2D小游戏
+
+Unity版本：2019.4.0f1
+
+用作Unity操作学习
